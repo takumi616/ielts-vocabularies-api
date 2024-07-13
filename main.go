787ba7eb869b6d527a65fbe8b5104dd84161d334
takumi616 <ohohyeah321@icloud.com>
2024-07-13 09:53:52 +0900

    Set up postgres db connection with gorm.

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/takumi616/ielts-vocabularies-api/infrastructures"
+	"github.com/takumi616/ielts-vocabularies-api/infrastructures/database"
 )
 
 func test(w http.ResponseWriter, r *http.Request) {
+	cfg, _ := infrastructures.NewConfig()
+
+	database.Open(context.Background(), cfg.PgConfig)
 	fmt.Fprintf(w, "test handler with request path: %s", r.URL.Path)
 }
 
